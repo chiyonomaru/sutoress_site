de784b2ca23a0cd7ea30a5a40150f3a486e87d33
ゆい <82437352+chiyonomaru@users.noreply.github.com>
2023-11-16 21:43:07 +0900

    Update 8-10.js

--- js/8-10.js
+++ js/8-10.js
@@ -64,16 +64,3 @@ $(function () {
 });
 
 
-function toggleNav() {
-  var body = document.body;
-  var hamburger = document.getElementById('js-hamburger');
-  var blackBg = document.getElementById('js-black-bg');
-
-  hamburger.addEventListener('click', function() {
-    body.classList.toggle('nav-open');
-  });
-  blackBg.addEventListener('click', function() {
-    body.classList.remove('nav-open');
-  });
-}
-toggleNav();
